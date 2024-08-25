6c2003845556acab118d757cd5e516cf083ea175
GuillermoSR <81838049+guillesr94@users.noreply.github.com>
2024-08-24 22:43:13 -0300

    ruta de importación en Contact.jsx corregida

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
-import Form from '../Components/Form/form'
+import Form from '../Components/Form/Form'
 import Footer from '../Components/Footer/Footer'
 import Banner from '../Components/Banner/Banner'
 import "../assets/css/style-liberty.css";
